af4b669deac3bce700ed7aed5ed5716e4ab889e1
Gowtham Reddy Ambati <gowthamreddy.ambati@gmail.com>
2018-12-12 12:24:54 +0530

    Created New GradeBook Types

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook:BaseGradeBook
+    {
+        public StandardGradeBook(string Name):base(Name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
