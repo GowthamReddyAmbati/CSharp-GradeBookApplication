25484daa457e2db35d76b322efa2608711b5f3a4
Gowtham Reddy Ambati <gowthamreddy.ambati@gmail.com>
2018-12-12 12:42:15 +0530

    Update RankedGradeBook.cs
    Ranked GradeBook with Letter Grade.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,7 @@
 using GradeBook.Enums;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +11,38 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(String name):base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count<5)
+            {
+                throw new InvalidOperationException();
+            }
+            var threshold = (int)Math.Ceiling(Students.Count * 0.20);
+            var grades = Students.OrderByDescending(e=>e.AverageGrade).Select(e=>e.AverageGrade).ToList();
+            if (grades[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+                return 'F';
+            
         }
     }
 }
